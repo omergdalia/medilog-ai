36038b0d22d2792aa1ed788eb81affb1fadfac11
omergdalia <omergdalia@gmail.com>
2025-05-29 22:48:19 +0300

    FIx BE & FE integration problems

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,49 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-// import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true
+import path from 'path';
+import fs from 'fs';
+
+function loadSharedEnv(envPath: string): Record<string, string> {
+  const envFile = fs.readFileSync(envPath, 'utf-8');
+  const lines = envFile.split('\n');
+
+  const mapped: Record<string, string> = {};
+  for (const line of lines) {
+    if (!line.trim() || line.trim().startsWith('#')) continue;
+    const [key, value] = line.split('=');
+    if (key && value) {
+      mapped[`VITE_${key.trim()}`] = value.trim();
     }
   }
-})
+  return mapped;
+}
 
 
-// TODO: Uncomment and use this if you need to load environment variables from a .env file
-// export default defineConfig(({ mode }) => {
-//     const env = loadEnv(mode, '.', '');
-//     return {
-//       define: {
-//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-//       },
-//       resolve: {
-//         alias: {
-//           '@': path.resolve(__dirname, '.'),
-//         }
-//       }
-//     };
-// });
+export default defineConfig(({mode}) => {
+  const sharedEnv = loadSharedEnv(path.resolve(__dirname, '../.env'));
+  const sharedEnv2 = loadEnv(mode, path.resolve(__dirname, '..'));
+  console.log('Shared Environment Variables:', sharedEnv);
+  console.log('Mode Environment Variables:', sharedEnv2);
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true
+      }
+    },
+    define: {
+      // 'process.env.BACKEND_PORT': parseInt(sharedEnv.VITE_BACKEND_PORT) || 8000,
+      'process.env.API_BASE': JSON.stringify(`http://localhost:${sharedEnv.VITE_BACKEND_PORT || '8000'}/api`),
+    },
+  };
+});
+
+
+// resolve: {
+//   alias: {
+//     '@': path.resolve(__dirname, '.'),
+//   }
+// }
